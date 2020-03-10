f721573af8483453f46a4bdf2c9eea8b622832a3
William Lin <willlin1996@gmail.com>
2020-03-10 23:05:43 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # # Make your shoe class here!
 class Shoe
   attr_accessor :color, :size, :material, :condition
@@ -10,4 +11,13 @@ class Shoe
     @condition = "new"
     puts "Your shoe is as good as new!"
   end
+=======
+# Make your shoe class here!
+class Shoe 
+  def inititalize(brand)
+    @brand=brand
+  end
+  
+  attr_accessor :brand
+>>>>>>> 22eb97fe6c5c28b25ecd404458592952fbbb4772
 end
